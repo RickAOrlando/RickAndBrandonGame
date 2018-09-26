7188a57f2937cb6bd9d5e8210c0f92fed3267246
RickAOrlando <RickAOrlando85@gmail.com>
2018-09-26 19:03:03 -0400

    - fixed additional bug affecting customer when toggling between fields

--- src/rickandbrandongame/Player.java
+++ src/rickandbrandongame/Player.java
@@ -9,9 +9,41 @@ package rickandbrandongame;
  * @author Pussy Whisperer
  */
 public class Player {
+    private String name;
+    private String playerMarker;
     
     //Constructor
-    Player(){
+    Player(String name, String playerMarker){
+        this.name = name;
+        this.playerMarker = playerMarker;
         
     }//End constructor
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the playerMarker
+     */
+    public String getPlayerMarker() {
+        return playerMarker;
+    }
+
+    /**
+     * @param playerMarker the playerMarker to set
+     */
+    public void setPlayerMarker(String playerMarker) {
+        this.playerMarker = playerMarker;
+    }
 }//End Class
