0c91460da40fe406ce670d562ba2994a3efa5ced
RickAOrlando <RickAOrlando85@gmail.com>
2018-09-26 17:44:50 -0400

    - fixed additional bug affecting customer when toggling between fields

--- src/rickandbrandongame/RickAndBrandonGame.java
+++ src/rickandbrandongame/RickAndBrandonGame.java
@@ -21,6 +21,14 @@ public class RickAndBrandonGame {
      */
 
     public static void main(String[] args) {
-
-    }
-}
+        
+        
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new HomeMenu().setVisible(true);
+            }
+        });
+    }//End Main
+}//End Class
