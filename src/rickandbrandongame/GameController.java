796fb52cb826e517f10b1a70ea6f0cf4fae1de50
RickAOrlando <RickAOrlando85@gmail.com>
2018-09-26 18:53:36 -0400

    - fixed additional bug affecting customer when toggling between fields

---
+++ src/rickandbrandongame/GameController.java
@@ -0,0 +1,17 @@
+/*
+ * Last Updated: 0/0/2017
+ * Class description:
+ */
+package rickandbrandongame;
+
+/**
+ *
+ * @author Pussy Whisperer
+ */
+public class GameController {
+    
+    //Constructor
+    GameController(){
+        
+    }//End Contructor
+}//End Class
