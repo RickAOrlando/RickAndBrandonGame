45f9b900fca64c59671df3e2f40f0a7af4ff75b2
RickAOrlando <RickAOrlando85@gmail.com>
2018-09-26 19:22:39 -0400

    - fixed additional bug affecting customer when toggling between fields

--- src/rickandbrandongame/GameController.java
+++ src/rickandbrandongame/GameController.java
@@ -4,13 +4,25 @@
  */
 package rickandbrandongame;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Pussy Whisperer
  */
 public class GameController {
+    //Global variables
+    ArrayList playerList = new ArrayList();
+    
     //Constructor
     GameController(){
         
     }//End Contructor
+    
+    public void createPlayer(){
+        //user input for player name argument
+        //alternatly assign markers for player marker argument
+        Player player = new Player("player name","player marker"); 
+        playerList.add(player);
+    }
 }//End Class
